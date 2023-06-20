41257e0d12d0dc3e943ef19ee80727140cde04d5
bvpav <78971592+bvpav@users.noreply.github.com>
2023-06-20 21:37:18 +0300

    okay, now this is dumb wtf

--- src/app/upload/actions.ts
+++ src/app/upload/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { auth } from "@clerk/nextjs";
+import { getAuth } from "@clerk/nextjs/server";
 import { nanoid } from "nanoid";
 import { redirect } from "next/navigation";
 import invariant from "tiny-invariant";
@@ -9,14 +9,20 @@ import { db } from "~/db";
 import { posts } from "~/db/schema";
 import { getOptString } from "~/utils/form-data";
 import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+// HACK: clerk is fucking dogshit and its library functions dont work
+//       so here's me rewriting them:
+function actuallyWorkingAuth() {
+  const request = new NextRequest("https://hacktues.bg", {
+    headers: headers(),
+  });
+  return getAuth(request);
+}
 
 export async function createPost(data: FormData) {
-  const { userId } = auth();
+  const { userId } = actuallyWorkingAuth();
   if (!userId) throw new Error("Not logged in");
-
-  // console.log(headers());
-
-  // const userId = "TODO";
 
   const fileKey = getOptString(data, "fk");
   if (!fileKey) throw new Error("No file key");
