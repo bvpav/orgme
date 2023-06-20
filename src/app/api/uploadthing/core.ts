fc84b0055cfff60504ddb079ce878b93741f9851
bvpav <78971592+bvpav@users.noreply.github.com>
2023-06-20 18:23:09 +0300

    used clerk for image upload auth

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,8 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { getAuth } from "@clerk/nextjs/server";
 
 const f = createUploadthing();
-
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -10,14 +9,10 @@ export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
-      const user = await auth(req);
+      const { userId } = getAuth(req);
+      if (!userId) throw new Error("Unauthorized");
 
-      // If you throw, the user will not be able to upload
-      if (!user) throw new Error("Unauthorized");
-
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return { userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
