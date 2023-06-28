51e8b27f29ec707261233cb89ecea8279b0af2bb
bvpav <78971592+bvpav@users.noreply.github.com>
2023-06-28 20:46:32 +0300

    Added button component

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "~/utils/ui";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-semibold ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-800",
+  {
+    variants: {
+      variant: {
+        default: "bg-emerald-600 text-white hover:bg-emerald-600/90",
+        destructive: "bg-red-500 text-white hover:bg-red-500/90",
+        outline: "border border-gray-200 hover:bg-gray-100 hover:text-white",
+        secondary: "bg-gray-100/10 text-white hover:bg-gray-100/20",
+        ghost: "hover:bg-gray-100/10 text-white",
+        link: "text-white underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
