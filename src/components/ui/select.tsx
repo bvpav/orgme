fd468adde668386bbfb4d6265f9f9be19cb7e4cc
bvpav <78971592+bvpav@users.noreply.github.com>
2023-06-29 21:43:49 +0300

    removed ugly focus ring (a11y issue tho)

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -20,7 +20,7 @@ const SelectTrigger = React.forwardRef<
     ref={ref}
     className={cn(
       // HACK: !bg-... because some dumb stylesheet overrides it somehow :/
-      "flex h-10 w-full items-center justify-between rounded-md !bg-white/10 px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+      "flex h-10 w-full items-center justify-between rounded-md !bg-white/10 px-3 py-2 text-sm placeholder:text-gray-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
       className
     )}
     {...props}
