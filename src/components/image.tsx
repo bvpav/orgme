3ff9a7f0eaf3ed0cad85c4890f695653aea3cfdc
bvpav <78971592+bvpav@users.noreply.github.com>
2023-07-01 23:28:36 +0300

    made delete menu item red

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -272,7 +272,7 @@ export const ImageDropdownMenu: React.FC<
           <DropdownMenuSeparator />
           {isOwner ? (
             <AlertDialogTrigger asChild>
-              <DropdownMenuItem className="gap-2">
+              <DropdownMenuItem className="gap-2 text-red-500">
                 <TbTrash /> Delete
               </DropdownMenuItem>
             </AlertDialogTrigger>
