bef6b3279ce26757e5b09560fb221c880e80a7a2
bvpav <78971592+bvpav@users.noreply.github.com>
2023-07-02 12:43:07 +0300

    made select trigger the same color as the button

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md bg-white/10 px-3 py-2 text-sm placeholder:text-gray-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+      "flex h-10 w-full items-center justify-between rounded-md bg-gray-100/10 px-3 py-2 text-sm placeholder:text-gray-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
       className
     )}
     {...props}
